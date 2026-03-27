70275f2ca0fb78d4795ee9cb0a24ed5663abbe8a
Egor <159301886+yui1337@users.noreply.github.com>
2026-03-27 12:20:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"<h1>Hello! App is running and updating via Webhooks! <br> Russian lang coming soon <br> Ведутся строительные работы</h1>")
+        self.wfile.write("<h1>Hello! App is running and updating via Webhooks! <br> Russian lang coming soon <br> Ведутся строительные работы</h1>")
 
 if __name__ == "__main__":
     server = HTTPServer(('0.0.0.0', 8181), Handler)
